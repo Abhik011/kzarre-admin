1f9f0b43f76e4d1da03b60fe0d459d63ab8c4967
Abhijeet kulkarni <kulkarniabhi011@gmail.com>
2025-11-08 14:19:08 +0530

    remove the capital mistake

--- app/ecom/page.tsx
+++ app/ecom/page.tsx
@@ -1,4 +1,4 @@
-import ECommerceSection from '@/Components/Ecommercesection';
+import ECommerceSection from '@/components/Ecommercesection';
 
 export default function UserManagementPage() {
   return <ECommerceSection />;
